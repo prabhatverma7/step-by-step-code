d1fc7e7dc8a2c51a609ddeb9af6bb6437a2cae3a
prabhat.kumar.verma <prabhatkmr626@gmail.com>
2023-01-11 11:30:10 +0530

    simple server creation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 const app = require('./app');
+const http = require('http');
 
+http.createServer((req,res)=>{
+    res.writeHead(200,{'Content-Type':'application/json'});
+    res.write(JSON.stringify({name:'prabhat',email:'prabhat@test.com'}));
+    res.end();
+}).listen(5000);
 console.log("hello",app.x);
-console.log("Import",app.z("Prabhat"));
+console.log("Import",app.z("Prabhat verma"));

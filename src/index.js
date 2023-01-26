fd9d23a0ce343fdcc21dd642a38a86991eaa2a77
prabhat.kumar.verma <prabhatkmr626@gmail.com>
2023-01-27 03:43:16 +0530

    expressjs basic api added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
-const app = require('./app');
-const http = require('http');
+// const app = require('./app');
+// const http = require('http');
 
-http.createServer((req,res)=>{
-    res.writeHead(200,{'Content-Type':'application/json'});
-    res.write(JSON.stringify({name:'prabhat',email:'prabhat@test.com'}));
-    res.end();
-}).listen(5000);
-console.log("hello",app.x);
-console.log("Import",app.z("Prabhat verma"));
+// http.createServer((req,res)=>{
+//     res.writeHead(200,{'Content-Type':'application/json'});
+//     res.write(JSON.stringify({name:'prabhat',email:'prabhat@test.com'}));
+//     res.end();
+// }).listen(5000);
+// console.log("hello",app.x);
+// console.log("Import",app.z("Prabhat verma"));
+
+const express = require('express');
+const app = express();
+
+app.get('',(req,res)=>{
+    console.log(req);
+    res.send('This is home page...');
+});
+
+app.get('/about',(req,res)=>{
+    res.send('This is about us page...');
+});
+
+app.listen(5000);
